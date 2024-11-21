d61c155273098f61cd1656169c8e47f3454d7c2e
lenoob <78706766+lenoobwastaken@users.noreply.github.com>
2024-11-20 19:10:15 -0500

    Add files via upload

---
+++ VRCLogger/Config.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRCLogger
+{
+    public static class Confwig
+    {
+        public class Config
+        {
+            public GeneralSettings GeneralSettings { get; set; }
+            public OverlaySettings OverlaySettings { get; set; }
+        }
+
+        public class GeneralSettings
+        {
+            public bool Vr { get; set; }
+            public bool AvatarLog { get; set; }
+            public bool JoinLog { get; set; }
+            public bool FirstTime { get; set; }
+
+        }
+
+        public class OverlaySettings
+        {
+            public int FontSize { get; set; }
+            public Position Position { get; set; }
+            public ColorSettings TextColor { get; set; }
+            public ColorSettings BackgroundColor { get; set; }
+
+        }
+        public class Position
+        {
+            public float X { get; set; }
+            public float Y { get; set; }
+            public float Z { get; set; }
+        }
+        public class ColorSettings
+        {
+            public int Red { get; set; }
+            public int Blue { get; set; }
+            public int Green { get; set; }
+            public int Alpha { get; set; }
+        }
+    }
+}
--- VRCLogger/Program.cs
+++ VRCLogger/Program.cs
@@ -14,47 +14,12 @@ using VRChat.API.Client;
 using VRChat.API.Model;
 using File = System.IO.File;
 using Newtonsoft.Json;
-
+using static VRCLogger.Confwig;
 namespace VRCLogger
 {
     public static class Program
     {
-        public class Config
-        {
-            public GeneralSettings GeneralSettings { get; set; }
-            public OverlaySettings OverlaySettings { get; set; }
-        }
-
-        public class GeneralSettings
-        {
-            public bool Vr { get; set; }
-            public bool AvatarLog { get; set; }
-            public bool JoinLog { get; set; }
-            public bool FirstTime { get; set; }
-
-        }
-
-        public class OverlaySettings
-        {
-            public int FontSize { get; set; }
-            public Position Position { get; set; }
-            public ColorSettings TextColor { get; set; }
-            public ColorSettings BackgroundColor { get; set; }
-
-        }
-        public class Position
-        {
-            public float X { get; set; }
-            public float Y { get; set; }
-            public float Z { get; set; }
-        }
-        public class ColorSettings
-        {
-            public int Red { get; set; }
-            public int Blue { get; set; }
-            public int Green { get; set; }
-            public int Alpha { get; set; }
-        }
+      
 
         public static List<string> PlayerList;
         public static List<string> logfile = new List<string>();
